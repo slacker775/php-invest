7cacd5f5d3ba932f77c9a3242867c4afbf107144
David Hollis <dhollis@davehollis.com>
2024-11-16 08:01:53 -0500

    Add return type for load() to satisfy some deprecation warnings

--- src/DataFixtures/InstrumentFixture.php
+++ src/DataFixtures/InstrumentFixture.php
@@ -18,7 +18,7 @@ class InstrumentFixture extends Fixture implements DependentFixtureInterface
         ];
     }
 
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
         $asset_manager = $manager->getRepository(Asset::class);
 
